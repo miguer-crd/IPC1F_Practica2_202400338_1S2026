1179f971357940fd67e6e2414a9711143c26c59d
miguer-crd <miguercorado7@gmail.com>
2026-03-24 18:08:57 -0600

    Código en proceso

---
+++ src/practica2/PRACTICA2.java
@@ -0,0 +1,13 @@
+package practica2;
+
+import practica2.views.VistaPrincipal;
+
+public class PRACTICA2 {
+
+
+    public static void main(String[] args) {
+       VistaPrincipal vPrincipal = new VistaPrincipal();
+       vPrincipal.setVisible(true); 
+    }
+    
+}
